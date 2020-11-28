d0e3e25df31cb04636b66c25ae040f631b1c224c
Yash-5865 <55460499+Yash-5865@users.noreply.github.com>
2020-11-28 16:14:23 +0530

    Theme changes

--- Aegis-master/app/src/main/java/com/example/aegis/Todo.java
+++ Aegis-master/app/src/main/java/com/example/aegis/Todo.java
@@ -1,14 +1,37 @@
 package com.example.aegis;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
 
 public class Todo extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
+        toolbar=findViewById(R.id.grocery_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("To Do List");
     }
+
 }
